e1c4d608ad9b0163bddafba9d7cd597f662d700b
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-09-07 11:55:45 +0100

    Updated the update_local_pdb function to make the file which the localpdb update function always errors on

--- steps/pdb_update.py
+++ steps/pdb_update.py
@@ -1,22 +1,49 @@
 import os
 import toml
 
+from functions.files import read_json
 
-def update_local_pdb(localpdb_path:str):
+import datetime 
+
+
+def update_local_pdb(localpdb_path:str, warehouse_path:str):
     """
     This function updates the localpdb instance and any installed plugins
 
     Args:
         localpdb_path(str): the path to the localpdb instance
+        warehouse_path(str): the path to the local copy of the histo.fyi warehouse
     """
+    version_info = read_json(f'{warehouse_path}/current_version.json')
+    current_version = version_info['version']
+
+
+    dateformat = '%Y%m%d'
+
+    current_version_date = datetime.datetime.strptime(current_version, dateformat)
+    next_version_date = current_version_date + datetime.timedelta(days=7)
+    next_version = next_version_date.strftime(dateformat)
+
+    version_path = f'{localpdb_path}/data/{next_version}'
+    version_file = f'{version_path}/obsolete.txt'
+
+    if not os.path.exists(version_file):
+        if not os.path.exists(version_path):
+            os.makedirs(version_path)
+        version_command = f'touch {version_file}'
+        os.system(version_command)
+        print (f'Creating version file for {next_version}')
+
     update_command = f'localpdb_setup -db_path {localpdb_path} --update'
+
+
     os.system(update_command)
 
 
 
 def main():
     config = toml.load('config.toml')
-    update_local_pdb(config['LOCALPDB_PATH'])
+    update_local_pdb(config['LOCALPDB_PATH'], config['WAREHOUSE_PATH'])
 
 
 main()
