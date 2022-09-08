34816e29615516b66d752f61e607fdfe85babc70
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-09-08 13:45:19 +0100

    Added functions to load an mmCif structure from a specified filepath and to load a set of pdb query files into a single list of pdb codes

---
+++ steps/functions/pdb.py
@@ -0,0 +1,29 @@
+from typing import List
+
+import gzip
+from Bio.PDB.MMCIFParser import MMCIFParser
+
+parser = MMCIFParser(QUIET=True)
+
+from .files import read_json
+
+
+def load_structure(pdb_code:str, pdb_filepath:str):
+    print (pdb_filepath)
+    try:
+        with gzip.open(pdb_filepath, 'rt') as f:
+            structure = parser.get_structure(pdb_code, f)[0]
+    except:
+        structure = None  
+    return structure
+
+
+def load_pdb_lists(mhc_class:str, warehouse_path:str, console) -> List:
+    console.rule(f'[bold]Loading pdb code lists for - {mhc_class}')
+    pdb_codes = []
+    for structure_type in ['class_i','truncated_class_i']:
+        print (f'Loading {structure_type}')
+        filepath = f'{warehouse_path}/queries/{structure_type}_hits.json'
+        json_data = read_json(filepath)
+        pdb_codes += [pdb_code for pdb_code in json_data]
+    return pdb_codes
