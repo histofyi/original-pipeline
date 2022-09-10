d6aa7eaf22ca479e366e9c08f39333045ee71f51
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-09-10 09:47:02 +0100

    Updated the parse_arg function to give more information in the CLI about what has been selected

--- steps/functions/cli.py
+++ steps/functions/cli.py
@@ -36,13 +36,15 @@ def parse_args(console):
     pdb_codes = []
 
     if pdb_code:
-        console.rule(f'[bold] Single PDB code mode - {pdb_code}')
+        console.print(f'[bold] Single PDB code mode - {pdb_code}')
         pdb_codes = [pdb_code]
     else:
         if mhc_class:
+            console.print(f'[bold] MHC class mode- {mhc_class}')
             pdb_codes = load_pdb_lists(mhc_class, config['WAREHOUSE_PATH'], console)
         elif set_slug and set_context:
             #TODO fill out this stub
+            console.print(f'[bold] Set mode- {set_context}/{set_slug}')
             members = []
             pdb_codes = members
 
