3e77f0ecda8e2101ddb7bed0e39e03a9231ce92a
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-09-13 19:23:49 +0100

    Added a type hint to slugify, added the deduplication function for a list

--- steps/functions/helpers.py
+++ steps/functions/helpers.py
@@ -1,9 +1,15 @@
+from typing import List, Dict, Tuple
 
-
-def slugify(string):
+def slugify(string:str) -> str:
     slug_char = '_'
     to_replace = [' ','-','.',',','[',']','{','}','(',')','/','\\','*',':']
     for replace_me in to_replace:
         if replace_me in string:
             string = string.replace(replace_me, slug_char)
     return string.lower()
+
+
+def deduplicate_list(pdb_code_list:List) -> List:
+    deduplicated_list = [pdb_code for pdb_code in set(pdb_code_list)]
+    sorted_list = sorted(deduplicated_list)
+    return sorted_list
