5c5f488240c4025317279c1d15507405fa64caa7
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-09-13 19:24:32 +0100

    Added a couple of fixes to assign species, also added progress indicator

--- steps/assign_species.py
+++ steps/assign_species.py
@@ -22,7 +22,7 @@ def get_species_info(organism_scientific:str, species:Dict) -> Dict:
         organism_info = {
             'scientific_name':species[organism_slug]['scientific_name'],
             'common_name':species[organism_slug]['common_name'],
-            'slug':organism_scientific
+            'slug':organism_slug
         }
     else:
         organism_info = None
@@ -38,7 +38,6 @@ mhc_class = 'class_i'
 
 pdb_codes = load_pdb_lists('class_i', config['WAREHOUSE_PATH'], console)
 
-#pdb_codes = ['1hhk']
 
 overrides = []
 assigned = []
@@ -46,33 +45,39 @@ missing = []
 
 facet_name = 'species'
 
-for pdb_code in pdb_codes:
-    species_info = load_facet(pdb_code, facet_name)
-    if not species_info:
-        species_info = None
-        if pdb_code in species_overrides:
-            overrides.append(pdb_code)
-            species_info = get_species_info(species_overrides[pdb_code]['organism_scientific_name'], species)
-            species_info['match_type'] = 'histo:override'
-        else:
-            assigned_chains = load_facet(pdb_code, 'assigned_chains')
-            print (pdb_code)
-            for chain in assigned_chains:
-                if chain in alpha_chains:
-                    molecules_info, success, errors = PDBeProvider(pdb_code).fetch_molecules()
-                    for molecule in molecules_info:
-                        if assigned_chains[chain]['chains'][0] in molecule['in_chains']:
-                            if 'source' in molecule:
-                                assigned.append(pdb_code)
-                                species_info = get_species_info(slugify(molecule['source'][0]['organism_scientific_name']), species)
-                                species_info['match_type'] = 'histo:assign_species'
+with Progress() as progress:
+        
+    task = progress.add_task("[white]Processing...", total=len(pdb_codes))
+
+    for pdb_code in pdb_codes:
+        species_info = load_facet(pdb_code, facet_name)
         if not species_info:
-            missing.append(pdb_code)
+            species_info = None
+            if pdb_code in species_overrides:
+                overrides.append(pdb_code)
+                species_info = get_species_info(species_overrides[pdb_code]['organism_scientific_name'], species)
+                species_info['match_type'] = 'histo:override'
+            else:
+                assigned_chains = load_facet(pdb_code, 'assigned_chains')
+                print (pdb_code)
+                for chain in assigned_chains:
+                    if chain in alpha_chains:
+                        molecules_info, success, errors = PDBeProvider(pdb_code).fetch_molecules()
+                        for molecule in molecules_info:
+                            if assigned_chains[chain]['chains'][0] in molecule['in_chains']:
+                                if 'source' in molecule:
+                                    assigned.append(pdb_code)
+                                    species_info = get_species_info(molecule['source'][0]['organism_scientific_name'], species)
+                                    species_info['match_type'] = 'histo:assign_species'
+            if not species_info:
+                missing.append(pdb_code)
+            else:
+                write_facet(pdb_code, facet_name, species_info)
         else:
-            write_facet(pdb_code, facet_name, species_info)
-    else:
-        assigned.append(pdb_code)
-    print (species_info)
+            assigned.append(pdb_code)
+        print (species_info)
+
+        progress.update(task, advance=1)
 
 print (f'{len(assigned)} assigned automatically')
 print (f'{len(overrides)} assigned using overrides')
