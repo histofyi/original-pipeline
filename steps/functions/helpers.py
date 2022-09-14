915bf663bcd47e40963b77141c069fa8740edf08
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-09-14 15:56:29 +0100

    Added helpers relating to parsing PDB date strings

--- steps/functions/helpers.py
+++ steps/functions/helpers.py
@@ -1,5 +1,7 @@
 from typing import List, Dict, Tuple
 
+import datetime
+
 def slugify(string:str) -> str:
     slug_char = '_'
     to_replace = [' ','-','.',',','[',']','{','}','(',')','/','\\','*',':']
@@ -13,3 +15,20 @@ def deduplicate_list(pdb_code_list:List) -> List:
     deduplicated_list = [pdb_code for pdb_code in set(pdb_code_list)]
     sorted_list = sorted(deduplicated_list)
     return sorted_list
+
+
+def parse_date_to_isoformat(datestring:str) -> str:
+    """
+        This function takes an 8 digit datestring from the PDBe return and delivers an ISO formated date string
+    """
+    try:
+        date = datetime.date(int(datestring[0:4]), int(datestring[4:6]), int(datestring[6:8]))
+        return date.isoformat()
+    except:
+        return datestring
+
+def parse_date_to_year(datestring:str) -> str:
+    """
+        This function takes an 8 digit datestring from the PDBe return and delivers a four digit year as a string
+    """
+    return datestring[0:4]
