7dd007f11e443c85bd40fbdf418ec36a7697c689
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-11-10 19:48:58 +0000

    Added a check that the dataset from localpdb matches the sqllite db

--- steps/build_indexes.py
+++ steps/build_indexes.py
@@ -1,24 +1,35 @@
 from typing import Dict, List
 
 from common.providers import httpProvider
+from rich.progress import Progress
+from rich.console import Console
 
+from functions.pdb import load_pdb_lists
+from functions.cli import load_config
+from functions.files import write_json
 
+import json
+
+config = load_config()
+console = Console()
 
 def rank_data_by_field(field:str, ascending:bool=True) -> List:
-
+    if ascending:
+        asc_desc = 'asc'
+    else:
+        asc_desc = 'desc'
+    print (field)
+    print (asc_desc)
     baseurl = 'http://127.0.0.1:8001/core.json?sql='
     select = 'select+pdb_code+from+core+'
     if field is not None:
         if field in ['deposition_date','pdb_code','resolution','revision_date','release_date']:
-            if ascending:
-                asc_desc = 'asc'
-            else:
-                asc_desc = 'desc'
+            
             order_by = f'order+by+{field}+{asc_desc}'
             url = f'{baseurl}{select}{order_by}'
-            print (url)
             raw_return = httpProvider().get(url, 'json')
             pdb_codes = parse_datasette_return(raw_return['rows'])
+            print (f'{len(pdb_codes)} in index')
             return pdb_codes
     else:
         print ('Field is None')
@@ -27,8 +38,13 @@ def rank_data_by_field(field:str, ascending:bool=True) -> List:
 
 def parse_datasette_return(raw_return:Dict) -> List:
     pdb_codes = [pdb_code[0] for pdb_code in raw_return]
-    print (len(pdb_codes))
     return pdb_codes 
+
+
+
+pdb_codes = load_pdb_lists('class_i', config['WAREHOUSE_PATH'], console)
+
+print (f'{len(pdb_codes)} records in the current dataset')
 
 indexes = {}
 
@@ -37,9 +53,21 @@ for field in ['pdb_code','resolution','deposition_date','revision_date','release
     indexes[index_name] = rank_data_by_field(field)
     index_name = f'{field}_desc'
     indexes[index_name] = rank_data_by_field(field, ascending=False)
-    
-    
-print (indexes)
+
+
+stringified_pdb_codes = spaced_stringify(pdb_codes)
+
+for index in indexes:
+    difference  = set(pdb_codes).difference(set(indexes[index]))
+    if len(difference) > 0:
+        print ('Reload the core.db with any updated data')
+    else:
+        print ('Matching sets')
+
+
+
+index_filepath = f'{config["WAREHOUSE_PATH"]}/datacompilations/index.json'
+write_json(index_filepath, indexes, verbose=True)
 
 
 
