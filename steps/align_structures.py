a79132a89d422b4e40570a11664bc18c9efde1da
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-11-28 11:01:49 +0000

    Added working function of align_structures pipeline action

--- steps/align_structures.py
+++ steps/align_structures.py
@@ -1,30 +1,33 @@
 from typing import List
 from pymol import cmd
-import os
+import os, sys
 
 import datetime
 
+from functions.cli import load_config
+from functions.files import load_facet, write_facet
 
-folder = 'edited'
 
 
-def align_to_canonical(mhc_class:str, pdb_code:str, assembly_id:int):
-    print ('-----')
-    print (pdb_code)
-    cmd.load(f'input/{folder}/{pdb_code}_{assembly_id}.cif', quiet=1)
-    cmd.save(f'output/{mhc_class}/with_solvent/raw/{pdb_code}_{assembly_id}.pdb')
 
-    cmd.load('canonical/cannonical_class_i_1hhk.pdb', quiet=1)
+
+def align_to_canonical(mhc_class:str, assembly_name:str, input_folder:str, output_folder:str, warehouse_path:str):
+    # we get CIF files from pdbe
+    cmd.load(f'{input_folder}/{assembly_name}.cif', quiet=1)
+    # but we also want PDB files, so save a copy now
+    cmd.save(f'{input_folder}/{assembly_name}.pdb')
+
+    cmd.load(f'{warehouse_path}/structures/canonical/cannonical_class_i_1hhk.pdb', quiet=1)
     
 
-    align = cmd.cealign('cannonical_class_i_1hhk',pdb_code)
+    align = cmd.cealign('cannonical_class_i_1hhk',assembly_name)
     align['rmsd'] = align['RMSD']
     del align['RMSD']
     cmd.delete('cannonical_class_i_1hhk')
     print (align['rmsd'])
     file_types = ['pdb', 'cif']
     for file_type in file_types:
-        file_name = f'output/{mhc_class}/with_solvent/aligned/{pdb_code}_{assembly_id}.{file_type}'
+        file_name = f'{output_folder}/{assembly_name}.{file_type}'
         cmd.save(file_name)
     return align
 
@@ -34,55 +37,67 @@ def iterator_cleanup():
 
 
 
-def action_cleanup():
+def action_cleanup(pipeline_path:str):
+    cif_files = [file_name for file_name in os.listdir(pipeline_path) if '.cif' in file_name]
+    for cif_file in cif_files:
+        os.remove(f'{pipeline_path}/{cif_file}')
+        print (f'Local {cif_file} removed')
     pass
 
 
-def perform_action(to_process:List, mhc_class:str):
+def perform_action(to_process:List, mhc_class:str, input_folder:str, output_folder:str, warehouse_path:str, pipeline_path:str):
     """
     Iterates through the list of items to process and performs the action
     """
     step_errors = []
+    facet_name = 'alignment'
     for assembly_name in to_process:
-        try:
-            assembly_id = assembly_name.split('_')[1]
-            pdb_code = assembly_name.split('_')[0]
-        except:
-            assembly_id = None
-            pdb_code = None
-            item_errors.append({'assembly_name':assembly_name, 'error':'split_error'})
-        try:
+        assembly_id = assembly_name.split('_')[1]
+        pdb_code = assembly_name.split('_')[0]
+        alignment_dict = load_facet(pdb_code, facet_name)
+        if alignment_dict is None:
             alignment_dict = {}
-            item_errors = []
+
+        print ('-----')
+        print (pdb_code)
+        item_errors = []
+        if not assembly_id in alignment_dict:
             if pdb_code:
-                alignment = align_to_canonical(mhc_class, pdb_code, assembly_id)
+                alignment = align_to_canonical(mhc_class, assembly_name, input_folder, output_folder, warehouse_path)
                 aligned = {
                     'aligned_on': mhc_class,
                     'alignment': alignment,
                     'last_updated': datetime.datetime.now().isoformat()
                 }
                 alignment_dict[assembly_id] = aligned
-        except:
-            item_errors.append({'assembly_name':assembly_name, 'error':'alignment_error'})
-        iterator_cleanup()
-        print (alignment_dict)
+            iterator_cleanup()
+            print (alignment_dict)
 
-        # write aligned facet
+            # write aligned facet
+            write_facet(pdb_code, facet_name, alignment_dict)
+
+
 
         if len(item_errors) > 0:
             for error in item_errors:
                 step_errors.append(error)
-    action_cleanup()
+    action_cleanup(pipeline_path)
     return step_errors
 
 
 
 def main():
-    structures = [file.split('.')[0] for file in os.listdir(f'input/{folder}')]
-    #pdb_codes = ['1hhk_1', '1hhk_2']
-    print (structures)
-    step_errors = perform_action(structures, 'class_i')
-    print (step_errors)
+    config = load_config()
+    input_folders = ['raw','edited']
+    for folder in input_folders:
+        input_folder = f'{config["WAREHOUSE_PATH"]}/structures/coordinates/public/class_i/with_solvent/{folder}'
+        output_folder = f'{config["WAREHOUSE_PATH"]}/structures/coordinates/public/class_i/with_solvent/aligned'
+        structures = [file.split('.')[0] for file in os.listdir(input_folder)]
+        print (structures)
+        step_errors = perform_action(structures, 'class_i', input_folder, output_folder, config['WAREHOUSE_PATH'], config['PIPELINE_PATH'])
+        print (step_errors)
+
+    
 
 
 main()
