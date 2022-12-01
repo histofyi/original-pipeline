e5fe3e755804df4bae29f6d94e69ede97ec78f85
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-12-01 18:24:53 +0000

    Added functions to detail the peptide extraction output
    #TODO find and fix the erroring pdb_codes
    
    ['7ndt_1', '7ndt_2', '3m17_4', '6gb5_1', '6gb7_2', '3m17_1', '7pdw_1', '1ldp_1', '6gb7_1', '6gb5_2', '3m17_2', '3m17_3', '5grg_1', '7ndu_1', '7ndq_1', '4lcy_2', '4lcy_1', '1nan_1', '7tud_1', '7pbc_1', '3m1b_2', '3m1b_1']

--- steps/extract_peptides.py
+++ steps/extract_peptides.py
@@ -9,9 +9,12 @@ from functions.files import load_facet, write_facet
 
 from Bio.PDB import PDBParser, MMCIFParser, Select
 from Bio.PDB.mmcifio import MMCIFIO
+from Bio.PDB.PDBIO import PDBIO
+
+
 
 class SelectChains(Select):
-    """ Only accept the specified chains when saving. """
+    """ Only accept the specified chains when saving."""
     def __init__(self, chain):
         self.chain = chain
 
@@ -28,8 +31,10 @@ class NonHetSelect(Select):
 
 
 def extract_peptides(peptide_chains:str, assembly_name:str, input_folder:str, warehouse_path:str):
-    solvated_folder = f'{warehouse_path}/structures/coordinates/public/class_i/with_solvent/peptide'
-    solvent_free_folder = f'{warehouse_path}/structures/coordinates/public/class_i/without_solvent/peptide'
+    filetype = 'peptide'
+    
+    solvated_folder = f'{warehouse_path}/structures/coordinates/public/class_i/with_solvent/{filetype}'
+    solvent_free_folder = f'{warehouse_path}/structures/coordinates/public/class_i/without_solvent/{filetype}'
 
     peptide_structure_details = {}
 
@@ -38,9 +43,7 @@ def extract_peptides(peptide_chains:str, assembly_name:str, input_folder:str, wa
     structure = PDBParser(PERMISSIVE=1).get_structure(assembly_name, input_filename)
 
     coordinate_server_url = 'https://coordinates.histo.fyi/structures/view/class_i/'
-    filetype_filename = f'{assembly_name}_peptide.pdb'
-
-    #https://coordinates.histo.fyi/structures/view/class_i/with_solvent/1hhk_1_aligned.pdb
+    filetype_filename = f'{assembly_name}_{filetype}'
 
 
     for chain in structure.get_chains():
@@ -48,29 +51,48 @@ def extract_peptides(peptide_chains:str, assembly_name:str, input_folder:str, wa
             chain_id = chain.id
             print (chain)
 
-            filename = f'{solvated_folder}/{assembly_name}.cif'
             
-            io = MMCIFIO()
-            io.set_structure(structure)
-            io.save(filename, SelectChains(chain_id))
+            mmcif_io = MMCIFIO()
+            pdb_io = PDBIO()
 
-            structure = MMCIFParser(QUIET=True).get_structure(assembly_name, filename)
+            export_formats = {
+                'cif':mmcif_io,
+                'pdb':pdb_io
+            }
+
+            for format in export_formats:
+                filename = f'{solvated_folder}/{assembly_name}.{format}'
+                io = export_formats[format]
+                io.set_structure(structure)
+                io.save(filename, SelectChains(chain_id))
+
+            peptide_only_filename = f'{solvated_folder}/{assembly_name}.cif'
             
-            filename = f'{solvent_free_folder}/{assembly_name}.cif'
+            try:
+                structure = MMCIFParser(QUIET=True).get_structure(assembly_name, peptide_only_filename)
             
-            io.set_structure(structure)
-            io.save(filename, NonHetSelect())
+                for format in export_formats:
+                    filename = f'{solvent_free_folder}/{assembly_name}.{format}'
+                    io = export_formats[format]
+                    io.set_structure(structure)
+                    io.save(filename, NonHetSelect())
 
-            peptide_structure_details['chain'] = chain_id
-            peptide_structure_details['with_solvent'] = f''
-            peptide_structure_details['without_solvent'] = f''
+                peptide_structure_details = {
+                    'chain':chain_id,
+                    'formats':{
+                        'cif':{},
+                        'pdb':{}
+                    }
+                }
+                for format in ['cif','pdb']:
+                    for solvent in ['with_solvent', 'without_solvent']:
+                        peptide_structure_details['formats'][format][solvent] = f'{coordinate_server_url}/{solvent}/{filetype_filename}.{format}'
+            except:
+                peptide_structure_details = {}
+    if len(peptide_structure_details) == 0:
+        peptide_structure_details = None
 
-    print (input_filename)
-    
-    
-
-
-    return {}
+    return peptide_structure_details
 
 
 def iterator_cleanup():
@@ -93,19 +115,34 @@ def perform_action(to_process:List, mhc_class:str, input_folder:str, warehouse_p
     step_errors = []
     facet_name = 'peptide_structures'
     for assembly_name in to_process:
+        print ('------')
+        print (assembly_name)
+        
         assembly_id = assembly_name.split('_')[1]
         pdb_code = assembly_name.split('_')[0]
+
         peptide_structures = load_facet(pdb_code, facet_name)
         peptide_details = load_facet(pdb_code, 'peptide_details')
-        print ('------')
+        
         print (pdb_code)
 
         if peptide_details:
             peptide_chains = [chain for chain in peptide_details]
-            print (peptide_chains)
             if peptide_structures is None:
-                peptide_structures = extract_peptides(peptide_chains, assembly_name, input_folder, warehouse_path)
+                peptide_structures = {}
+            if not assembly_id in peptide_structures:
+                peptide_assembly_info = extract_peptides(peptide_chains, assembly_name, input_folder, warehouse_path)
 
+                if peptide_assembly_info is not None:
+                    peptide_structures[assembly_id] = peptide_assembly_info
+
+                print (peptide_structures)
+                if len(peptide_structures) > 0:
+                    write_facet(pdb_code, facet_name, peptide_structures)
+                else:
+                    if assembly_name not in step_errors:
+                        step_errors.append(assembly_name)
+                
             else:
                 print ('ALREADY SPLIT')
         else:
@@ -119,8 +156,8 @@ def main():
     config = load_config()
     
     input_folder = f'{config["WAREHOUSE_PATH"]}/structures/coordinates/public/class_i/with_solvent/aligned'
-    structures = [file.split('.')[0] for file in os.listdir(input_folder)]
-    structures = ['1hhk_1','1hhk_2']
+    structures = [file.split('.')[0] for file in os.listdir(input_folder) if len(file.split('.')[0]) > 0]
+    #structures = ['1hhk_1','1hhk_2']
     print (structures)
     step_errors = perform_action(structures, 'class_i', input_folder, config['WAREHOUSE_PATH'], config['PIPELINE_PATH'])
     print (step_errors)
